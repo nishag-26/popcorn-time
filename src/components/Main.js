e0d16b7741f9e1834a8c1f397dbd931cbef01a89
Nishtha Agarwal <nishtha.agarwal26@gmail.com>
2023-06-21 10:17:59 +0200

    second commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,65 @@
+import { useState } from "react";
+import movies from "../data/movies.json";
+
+import "./Main.css";
+
 function Main() {
-    return(
-        <h1> This is the Main component </h1> 
+
+
+    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+    
+
+    const deleteMovie = (movieId) => {
+        // console.log("deleting movie with id....", movieId)
+
+        // moviesToDisplay.push(); // NEVER MODIFY STATE DIRECTLY !
+
+        const newList = moviesToDisplay.filter( (element) => {
+            return element.id !== movieId;
+        });
+
+        setMoviesToDisplay(newList);
+
+    }
+
+
+    // Conditional Rendering: option A (Element Variables)
+    let message = "";
+    if(moviesToDisplay.length > 0){
+        message = <h1>Number of movies: {moviesToDisplay.length}</h1>;
+    } else {
+        message = <h1>Sorry, no movies to display</h1>;
+    }
+
+
+    return (
+        <div className="Main">
+
+            {message}
+
+            {moviesToDisplay.map((movieObj) => {
+                return(
+                    <div key={movieObj.id} className="card">
+                        <p>{movieObj.title}</p>
+                        <p>Rating: {movieObj.rating}</p>
+
+
+                        { movieObj.imgURL 
+                            ? <img src={movieObj.imgURL} /> 
+                            : <img src="https://dummyimage.com/182x268/ffffff/000000" />
+                        }
+
+
+                        { movieObj.rating > 8 && <p>RECOMMENDED</p>}
+
+                        <p>
+                            <button onClick={ () => {deleteMovie(movieObj.id)}}>Delete this movie</button>
+                        </p>
+                    </div>
+                )
+            })}
+        </div>
     );
 }
+
 export default Main;
